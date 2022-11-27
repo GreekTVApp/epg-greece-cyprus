2d7bf34e065297209a06e5ed76a27c477ee26741
Nikolas Spiridakis <30593419+1nikolas@users.noreply.github.com>
2022-11-27 12:49:50 +0200

    Fix DST
    ooops hardcoded +3 for some reason

--- xmlutil.py
+++ xmlutil.py
@@ -13,6 +13,8 @@ def push(channel_name, data):
     append('<channel id="{}">'.format(channel_name))
     append('<display-name>{}</display-name>'.format(escape(channel_name)))
     append('</channel>')
+    
+    timezone = datetime.now(pytz.timezone('Europe/Athens')).strftime('%z')
 
     for channel in data:
         start = datetime.fromtimestamp(channel['start_time'], pytz.timezone('Europe/Athens')).strftime('%Y%m%d%H%M%S')
@@ -20,9 +22,9 @@ def push(channel_name, data):
         stop = ''
 
         if 'end_time' in channel:
-            stop = 'stop="{} +0300"'.format(datetime.fromtimestamp(channel['end_time'], pytz.timezone('Europe/Athens')).strftime('%Y%m%d%H%M%S'))
+            stop = 'stop="{} {}"'.format(datetime.fromtimestamp(channel['end_time'], pytz.timezone('Europe/Athens')).strftime('%Y%m%d%H%M%S'), timezone)
 
-        append('<programme start="{} +0300" {} channel="{}">'.format(start, stop, channel_name))
+        append('<programme start="{} {}" {} channel="{}">'.format(start, timezone, stop, channel_name))
         append('<title lang="el">{}</title>'.format(escape(channel['title'])))
         append('<desc>{}</desc>'.format(escape(channel['description'])))
         append('</programme>')
